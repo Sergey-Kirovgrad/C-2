7dfc05eddf1632be39cda4fd7fcaace57667b975
Sergey <arakcheev1@yandex.ru>
2022-12-23 19:33:38 +0500

    add zadacha13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа
+//или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Введите любое число: ");
+int a = int.Parse(Console.ReadLine());
+if(a < 100){
+    Console.WriteLine("Число меньше 100");
+}else{
+    while(a > 1000){
+        a /=10;
+    }
+    Console.WriteLine(a % 10);
+}
